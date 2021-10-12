82160c3249403eb0cb348a40fd5d2f5d5213b874
Jurriaan Katsman PC <jurriaan@studiokatsman.nl>
2021-10-12 22:06:33 +0200

    3

---
+++ Leds_aansturing/Test_v_Micro/Test_v_Micro/Test_v_Micro.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Test_v_Micro.ino
+ Created:	10/12/2021 9:30:12 PM
+ Author:	jurri
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
